f095bd2ca73f05d44508d20a5cd7b7a0570e51e8
ziihoyam <hyunhyebinn@gmail.com>
2022-01-02 00:37:36 +0900

    + 최소수량 조건 추가 + css 레이아웃 수정

--- src/components/Container.js
+++ src/components/Container.js
@@ -17,7 +17,7 @@ class Container extends Component{
         console.log('Container 렌더링');
         const{ count } = this.state;
         const{ payResult } = this.state;
-
+         
         // 슬라이드 스크립트
         function slideColor(){
             var phoneImg=document.querySelector('.phone_imgs');
@@ -45,19 +45,28 @@ class Container extends Component{
                         <h1 className="big_txt">갤럭시 Z 플립3 5G</h1>
                         <p>SM-F711NLVWKOO</p>
                         <div className="plus_group">
-                            <button onClick={()=>{
-                                this.setState({count : count-1, payResult : payResult-1254000})
-                            }
-                            }>-</button>
+                            <button className="minus_btn" onClick={()=>{
+                                if(count>1){
+                                    this.setState({count : count-1, payResult : payResult-1254000})
+                                }
+                                else if(count==0 || count==1){
+                                    alert('최소 수량은 1개 입니다.');
+                                    console.log('이벤트 실행');
+                                    this.setState({count : 1, payResult : 1254000});
+                                };}}>-</button>
                             <span className="result">{this.state.count}</span>
-                            <button onClick={()=>{
-                                this.setState({count : count+1, payResult : payResult+1254000})
-                            }}>+</button>
+                            <button className="plus_btn" onClick={()=>{
+                                this.setState({count : count+1, payResult : payResult+1254000})}}>+</button>
                         </div>
                         <span>판매가  </span>
                         <span className="big_txt">1,254,000</span>
                         <span>원</span>
-                        <h3 className="plusMinusResult">합계 : { this.state.payResult.toLocaleString() }원</h3>
+                        <div className="sum_group">
+                            <span className="sum_hap">총 구매금액 </span>
+                            <span className="sum_result">
+                                { this.state.payResult.toLocaleString() }</span>
+                            <span className="sum_won">원</span>
+                        </div>
                         <div className="pay_button">
                             <button>바로구매</button>
                             <button>장바구니</button>
